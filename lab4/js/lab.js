f4884dc19dceb7f60e424f0d415ca05d4ad801de
cwispybean <efu6@ucsc.edu>
2023-04-23 16:17:41 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,26 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock Paper Scissors
+// An implementation of the game
+// Author: Evelyn Fu <efu6@ucsc.edu>
+// Date: April 2024
 
 // Constants
+
+// Pseudocode
+// Initialize variables for user_score and computer_score to 0.
+// Repeat the following steps until the game ends:
+  // Ask the user to choose between rock, paper, or scissors.
+  // Generate a random choice for the computer.
+  // Compare the user's and computer's choices:
+      // If they choose the same, it's a tie.
+      // Otherwise, winner is determined as follows:
+          // Rock beats scissors.
+          // Scissors beat paper.
+          // Paper beats rock.
+          // If the user wins, add 1 to user_score. Otherwise, add 1 to computer_score.
+  // Print the result of the round.
+// Print the final result of the game.
+
 
 // Functions
 
