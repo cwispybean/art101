167ed2b5081891979cb7f7d40d8f35b43332c914
cwispybean <efu6@ucsc.edu>
2023-05-01 23:26:46 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * index.js - experiment with JavaScript arrays and objects
+ * Author: Evelyn Fu
+ * Date: 2 May 2023
+ **/
+
+// Define Variables
+myTransport = ["Car", " Bike", " Bus"];
+
+// Create Object for myMainRide
+myMainRide = {
+  make: "Toyota",
+  model: "Prius",
+  color: "Silver",
+  year: 2021,
+  // Method, define subject in object
+  age: function() {
+      return 2023 - this.year;
+  }
+}
+
+// Output
+document.writeln("Forms of Transportation I use: ", myTransport, "<br>");
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // Constants
 
