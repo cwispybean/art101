fe6b169044e5b7182188422982414ceb5acbe523
cwispybean <efu6@ucsc.edu>
2023-05-09 11:32:42 -0700

    Lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,23 +1,37 @@
 /**
- * index.js - write functions that take user input and manipulate a string
+ * index.js - working with my partner to experiment with anonymous functions and callbacks
  * Author: Evelyn Fu
- * Date: 4 May 2023
+ * Date: 9 May 2023
  **/
 
-// Constants
+// Array of Numbers
+array = [1, 2, 3, 4, 5];
+console.log("My Array: ", array)
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Function
+function square(x){
+  return (x * x);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Testing
+console.log("What is the square of 10?", square(10));
+console.log("What is the square of 15?", square(15));
 
-// let's get this party started
-main();
+var result = array.map(square);
+console.log("Square of Array: ", result)
+
+// Anonymous Function
+var results = array.map(function(x){
+  return x + 1;
+})
+
+// Test, should return 2, 3, 4, 5, 6
+console.log("Array with incremented numbers: ", results)
+
+// Print in HTML
+var outputEl = document.getElementById("output");
+outputEl.innerHTML = "My Array: " + array.join(", ") + "<br>" +
+                     "Square of 10: " + square(10) + "<br>" +
+                     "Square of 15: " + square(15) + "<br>" +
+                     "Square of Array: " + result.join(", ") + "<br>" +
+                     "Array with incremented numbers: " + results.join(", ");
