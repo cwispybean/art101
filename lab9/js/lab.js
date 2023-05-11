b479e29c58c99186e169791f6f88f6df88ef5703
cwispybean <efu6@ucsc.edu>
2023-05-10 23:24:09 -0700

    Lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,8 +1,34 @@
 /**
- * index.js - write functions that take user input and manipulate a string
+ * index.js - experimenting with DOM manipulation
  * Author: Evelyn Fu
- * Date: 4 May 2023
+ * Date: 11 May 2023
  **/
+
+// Find the output <div> and assign it to a variable
+var outputEl = document.getElementById("output");
+
+// Create a new element and assign it to a variable
+var new1El = document.createElement("p");
+
+// Set the HTML of the new element
+new1El.innerHTML = "Test Element no.1";
+
+// Append the new element to the output <div>
+outputEl.appendChild(new1El);
+
+// Create another new element and assign it to a variable
+var new2El = document.createElement("p");
+
+// Set the HTML of the second new element
+new2El.innerHTML = "Test Element no.2";
+
+// Append the second new element to the output <div>
+outputEl.appendChild(new2El);
+
+// Change the CSS attributes of at least two elements
+new1El.style.color = "#f6b3d3";
+new2El.style.backgroundColor = "#f180b7";
+
 
 // Constants
 
