7f681aa0ec0b256b0ee2628103f049019b97b7c2
cwispybean <efu6@ucsc.edu>
2023-05-24 22:50:11 -0700

    Lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,11 +1,52 @@
 /**
- * index.js - 
+ * index.js - experimenting with loops
  * Author: Evelyn Fu
- * Date: May 2023
+ * Date: 25 May 2023
  **/
 
+$(document).ready(function() {
 
+  //FizzBuzz Function {3: "Fizz", 5: "Buzz", 7: "Boom"}
+  function fizzBuzzBoom() {
+    $("#output").empty(); // Clears Previous Outputs
+    var max = parseInt($("#max").val());
 
+    for (var i = 1; i <= max; i++) {
+      var str = ""; // Initialize Empty String
+      
+      // Print Fizz if number is multiple of 3
+      if (i % 3 == 0) {
+        str += "Fizz";
+      }
+
+      // Print Buzz if number is multiple of 5
+      if (i % 5 == 0) {
+        str += "Buzz";
+      }
+
+      // Print Boom if number is multiple of 7
+      if (i % 7 == 0) {
+        str += "Boom";
+      }
+
+      // If string still empty, prints the number
+      if (str == "") {
+        str = i;
+      }
+
+      // Add "!" to Fizz Buzz Boom
+      else {
+        str += "!";
+      }
+      $("#output").append("<div>" + str + "</div>");
+    }
+  }
+
+  $("#submit").click(function() {
+    fizzBuzzBoom();
+    console.log("Button Clicked.");
+  });
+});
 
 // Main
 function main() {
