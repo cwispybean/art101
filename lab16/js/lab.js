eb90911c59d67cc386e9327f2a94b2c0319f8b4b
cwispybean <efu6@ucsc.edu>
2023-06-06 03:15:17 -0700

    Lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,10 +1,68 @@
 /**
- * index.js - 
+ * index.js - experimenting with processing JSON from an API
  * Author: Evelyn Fu
- * Date: May 2023
+ * Date: 6 June 2023
  **/
 
+// URL Variables
+var URL = "https://xkcd.com/info.0.json";
+var frontURL = "https://xkcd.com/";
+var endURL = "info.0.json";
 
+function comic(num) {
+  // Checking if comic number is provided, otherwise set it as an empty string
+  if (typeof num === 'undefined') {
+    numStr = "";
+  } else {
+    numStr = num.toString() + "/";
+  }
+  // Making new URL
+  var newURL = frontURL + numStr + endURL;
+  console.log("Current URL:", newURL);
+
+  // AJAX request to retrieve the comic data
+  $.ajax({
+    url: newURL,
+    type: "GET",
+    data: {},
+    dataType: "json",
+  })
+
+  .done(function(data) {
+    console.log(data);
+
+    // Data Extraction
+    var title = data.title;
+    var image = data.img;
+    var alt = data.alt; // Extracting alt text
+    var dataNum = data.num;
+
+    // Building the HTML
+    var output = "";
+    output += "<h3>" + title + "</h3>";
+    output += "<img src='" + image + "' title='" + alt + "'><br>";
+    output += "<button id='previous'>Previous</button> <button id='next'>Next</button>";
+    output += "</div>";
+
+    $("#output").html(output);
+
+    // Display comic HTML
+    $("#output").html(output);
+
+    // Previous Button Event Listener
+    $("#previous").click(function(){
+      comic(dataNum - 1);
+    });
+
+    // Next Button Event Listener
+    $("#next").click(function(){
+      comic(dataNum + 1);
+    });
+    
+  });
+}
+
+comic();
 
 
 // Main
@@ -13,4 +71,3 @@ function main() {
 }
 
 main();
-
